e2e6c4f639d97f68afe1538a6ffcff8109a5f1b9
Ana Margarida Borges <a252137@dac.unicamp.br>
2025-10-19 08:33:26 -0300

    query stats de sexualidade

--- queries.sql
+++ queries.sql
@@ -50,3 +50,56 @@ AND EXISTS (
   WHERE ap.IdUsuario = u.IdUsuario
   AND ap.IdGenero = u_logado.IdGenero
 );
+
+-- query para estatísticas de sexualidades
+-- nesse exemplo, eu calculo a porcentagem de matches
+-- envolvendo homens héteros em que o rating dele é menor que o da mulher
+WITH MatchesValidos AS (
+  -- quero pegar todos os matches envolvendo homens héteros
+    SELECT dm.IdDeuMatch,
+        CASE 
+            WHEN u1.IdGenero IN (3, 4) THEN u1.IdUsuario
+            ELSE u2.IdUsuario
+        END AS IdHomem,
+        CASE 
+            WHEN u1.IdGenero IN (3, 4) THEN u2.IdUsuario
+            ELSE u1.IdUsuario
+        END AS IdMulher
+    FROM DeuMatch dm
+    JOIN Usuario u1 ON u1.IdUsuario = dm.IdUsuarioDeuLike
+    JOIN Usuario u2 ON u2.IdUsuario = dm.IdUsuarioRecebeuLike
+   -- checo se o match seu certo e se ele envolve um homem e uma mulher
+    WHERE dm.MatchSucesso = TRUE
+      AND ((u1.IdGenero IN (1, 2) AND u2.IdGenero IN (3, 4))
+           OR (u1.IdGenero IN (3, 4) AND u2.IdGenero IN (1, 2)))
+  	-- se ele deu match com uma mulher, ele necessariamente gosta de mulheres
+    -- devo checar então se ele é hetero ou bi
+  	-- se ele for hetero, só existem instâncias dele no AtraidoPor relacionadas a mulheres
+      AND NOT EXISTS (
+          SELECT *
+          FROM AtraidoPor ap
+          WHERE ap.IdUsuario = 
+        	CASE
+        		WHEN u1.IdGenero IN (3, 4) THEN u1.IdUsuario
+        		ELSE u2.IdUsuario
+        	END
+            AND ap.IdGenero IN (3, 4, 5, 6)
+      )
+),
+PegarRatingDoMatch AS (
+    SELECT 
+        mv.*,
+        r_homem.Valor AS RatingHomem,
+        r_mulher.Valor AS RatingMulher
+    FROM MatchesValidos mv
+    JOIN Rating r_homem ON r_homem.IdUsuario = mv.IdHomem
+    JOIN Rating r_mulher ON r_mulher.IdUsuario = mv.IdMulher
+)
+SELECT 
+    COUNT(*) AS TotalMatches,
+    SUM(CASE WHEN RatingHomem < RatingMulher THEN 1 ELSE 0 END) AS MatchesHomemRatingMenor,
+    ROUND(
+        100.0 * SUM(CASE WHEN RatingHomem < RatingMulher THEN 1 ELSE 0 END) / COUNT(*),
+        2
+    ) AS PorcentagemHomemRatingMenor
+FROM PegarRatingDoMatch;
